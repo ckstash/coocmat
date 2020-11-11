6a8cc931aa39668e7cef421bf1bea8c78abe839a
ckstash <47305472+ckstash@users.noreply.github.com>
2020-11-11 13:33:12 +0100

    Update cooccurrence_matrix.R

--- R/cooccurrence_matrix.R
+++ R/cooccurrence_matrix.R
@@ -1,22 +1,22 @@
 #####
-# Given a document-topic matrix (rows are documents)
-# returns a topic cooccurrence matrix
+# Given a document-topic matrix (rows are documents) returns a topic cooccurrence matrix
+# R version 3.6
 #####
 
 get_cooccurrence_matrix <- function(df) {
-  #load libraries
+  # Load libraries
   library(magrittr)
   library(dplyr)
   
-  #get row and col indicies of non-zero elems as dataframe
-  #col names are "row" and "col"
+  # Get row and column indicies of non-zero elems as dataframe
+  # Column names are "row" and "col"
   df_nz <- as.data.frame(which(df != 0, arr.ind = T))
 
-  #get list of sorted documents (unique row indices) and topics (unique col indices)
+  # Get list of sorted documents (unique row indices) and topics (unique column indices)
   rows <- sort(unique(df_nz$row))
   cols <- sort(unique(df_nz$col))
 
-  #find cooccurring pairs (n choose 2 combinations)
+  # Find cooccurring pairs (n choose 2 combinations)
   df_comb_list <- list()
   for(i in rows) {
     tryCatch(
@@ -25,22 +25,22 @@ get_cooccurrence_matrix <- function(df) {
         filter(row == i) %>%
         select(col) %>%
         t() %>% combn(2) %>% t(),
-      #do nothing upon error, but NULL value will be added to df_comb_list
+      # Do nothing upon error, but NULL value will be added to df_comb_list
       error = function(e) {}
     )
   }
 
-  #list of non-NULL combo dfs
+  # List of non-NULL combo dfs
   df_comb_list <- df_comb_list[lengths(df_comb_list) != 0]
   #single df of combos
   df_combs <- as.data.frame(do.call(rbind, df_comb_list))
 
-  #initialize coocurrence matrix
+  # Initialize coocurrence matrix
   cooccurrence_matrix <- data.frame(matrix(0, ncol = length(cols), nrow = length(cols)))
   rownames(cooccurrence_matrix) <- cols
   colnames(cooccurrence_matrix) <- cols
 
-  #fill matrix
+  # Fill matrix
   for (i in seq(nrow(df_combs))) {
     rn <- toString(df_combs[i, 1]) #row name
     cn <- toString(df_combs[i, 2]) #col name
